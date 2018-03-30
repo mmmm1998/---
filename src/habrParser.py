f5e69bb9b38a0af985938360eaa6b5b7c2869ec8
Nikita Sirgienko <warquark@gmail.com>
2018-03-30 15:00:50 +0300

    habrParser refactoring: move xpath requests in separete map, one function is inlined

--- src/habrParser.py
+++ src/habrParser.py
@@ -5,10 +5,7 @@ import os
 import re
 from lxml.html import parse
 from lxml.builder import E
-from urllib.request import urlopen
-
-def _raw_views_count(xmlTree):
-    return xmlTree.find('//span[@class="post-stats__views-count"]').text    
+from urllib.request import urlopen 
 
 # 3k -> 3000
 # 10 -> 10
@@ -30,6 +27,15 @@ def _body2text(body):
     # TODO: Сделать преобразование тела поста в plain text
     return 'Not implemented, TODO body2text'
 
+_find_tags = {
+    'title': '//h1[@class="post__title post__title_full"]/span[@class="post__title-text"]',
+    'author': '//span[@class="user-info__nickname user-info__nickname_small"]',
+    'body': '//div[@class="post__text post__text-html js-mediator-article"]',
+    'rating': '//span[@class="voting-wjt__counter voting-wjt__counter_positive  js-score"]',
+    'comments count': '//strong[@class="comments-section__head-counter"]',
+    'views count': '//span[@class="post-stats__views-count"]',
+    'bookmarks count': '//span[@class="bookmark__counter js-favs_count"]'
+}
 def parseHabr(link):
     try:
         post = {
@@ -44,35 +50,36 @@ def parseHabr(link):
 
         data = parse(urlopen(link))
 
-        post['title'] = data.find('//h1[@class="post__title post__title_full"]/span[@class="post__title-text"]').text
+        post['title'] = data.find(_find_tags['title']).text
         try:
-            post['author'] = data.find('//span[@class="user-info__nickname user-info__nickname_small"]').text
+            post['author'] = data.find(_find_tags['author']).text
         except: # TODO: test this error
             post['author'] = None
 
         try:
-            post['body'] = _body2text(data.find('//div[@class="post__text post__text-html js-mediator-article"]'))
+            post['body'] = _body2text(data.find(_find_tags['body']))
         except:
             post['body'] = None
 
         try:
-            post['rating'] = int(data.find('//span[@class="voting-wjt__counter voting-wjt__counter_positive  js-score"]').text)
+            post['rating'] = int(data.find(_find_tags['rating']).text)
         except: 
             post['rating']=None
 
         try:
             # TODO: А если комментариев > 1000, то будет число, или же 1k?
-            post['comments'] = int(data.find('//strong[@class="comments-section__head-counter"]').text)
+            post['comments'] = int(data.find(_find_tags['comments count']).text)
         except: 
             post['comments'] = None
 
         try:
-            post['views'] = _normalize_views_count(_raw_views_count(data))
+            raw_views = data.find(_find_tags['views count']).text
+            post['views'] = _normalize_views_count(raw_views)
         except:
             post['views'] = None
 
         try:
-            post['bookmarks'] = int(data.find('//span[@class="bookmark__counter js-favs_count"]').text)
+            post['bookmarks'] = int(data.find(_find_tags['bookmarks count']).text)
         except:
             post['bookmarks'] = None
 
